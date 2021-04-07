c1c9c96bb746a0d9083aef7af7f86cc6fb372959
ChoiysApple <chcs1370@gmail.com>
2021-04-07 11:49:31 +0900

    Init Project

---
+++ Care Dementia/Template/PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/PlaygroundPage.playgroundpage/main.swift
@@ -0,0 +1,19 @@
+//#-hidden-code
+//
+//  See LICENSE folder for this template’s licensing information.
+//
+//  Abstract:
+//  The Swift file containing the source code edited by the user of this playground book.
+//
+//#-end-hidden-code
+let str = "Hello, playground"
+
+let welcomeString = "Hello, World"
+
+//#-code-completion(everything, hide)
+//#-code-completion(identifier, show, setName(_:))
+
+//Set Your Name
+//#-editable-code
+
+//#-end-editable-code
