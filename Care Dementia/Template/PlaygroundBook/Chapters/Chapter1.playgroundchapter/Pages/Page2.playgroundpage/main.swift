616392419e2921c9ec71e68645bc65081d961285
ChoiysApple <chcs1370@gmail.com>
2021-04-12 11:35:43 +0900

    Update

---
+++ Care Dementia/Template/PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/Page2.playgroundpage/main.swift
@@ -0,0 +1,36 @@
+//#-hidden-code
+//
+//  See LICENSE folder for this template’s licensing information.
+//
+//  Abstract:
+//  The Swift file containing the source code edited by the user of this playground book.
+//
+//#-end-hidden-code
+
+/*:
+ # Welcome to CodersHigh Cruse
+ 
+ It's captain coding rabbit
+ 
+ This is the very first stage of your aboard
+
+ Follow the instruction
+ 
+ Hope you have a good chance to improve your coding skill here
+ 
+ 
+ 
+ ## Instruction
+ Type your name using setName() function
+ 
+ 
+     setName("CodingRabbit")
+ */
+
+//#-code-completion(everything, hide)
+//#-code-completion(identifier, show, setName(_:))
+
+//Set Your Name
+//#-editable-code
+
+//#-end-editable-code
